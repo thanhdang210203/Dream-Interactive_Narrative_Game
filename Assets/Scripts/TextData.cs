dc29a6c2e7b9d57c8d0697062a64fcbd0a44b2e7
ThanhDang <thanhdang210203@gmail.com>
2024-06-27 07:01:02 +0700

    thanhdang

---
+++ Assets/Scripts/TextData.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "TextData", menuName = "TextData", order = 1)]
+public class TextData : ScriptableObject
+{
+   public string[] PCTexts;
+}
